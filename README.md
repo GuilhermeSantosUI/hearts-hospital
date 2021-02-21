e6669dd21a3a84abade4bb8a7b0b2bf215134d5a
GuilhermeSantosUI <guilherme.contato080@gmail.com>
2021-02-21 01:15:21 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 align="center">
+	<img alt="Hearth's Hospital" src=".github/logo.png" width="200px" />
+</h1>
---
+++ src/application/Main.java
@@ -0,0 +1,34 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	private static Scene mainScene;
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			ScrollPane scrollPane = loader.load();
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			mainScene = new Scene(scrollPane);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Hearth's Hospital | Login");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+}
